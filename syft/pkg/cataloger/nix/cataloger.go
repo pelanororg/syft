f91084d748d75e0084da9cc0e839086b4dcd460a
Alex Goodman <alex.goodman@anchore.com>
2023-01-18 17:35:49 -0500

    add more robust nix store path parsing
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

---
+++ syft/pkg/cataloger/nix/cataloger.go
@@ -0,0 +1,62 @@
+package nix
+
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/bmatcuk/doublestar/v4"
+
+	"github.com/anchore/syft/syft/artifact"
+	"github.com/anchore/syft/syft/pkg"
+	"github.com/anchore/syft/syft/source"
+)
+
+const (
+	catalogerName = "nix-store-cataloger"
+	nixStoreGlob  = "/nix/store/*"
+)
+
+var (
+	numericPattern = regexp.MustCompile(`\d`)
+	// attempts to find the right-most example of something that appears to be a version (semver or otherwise)
+	// example input: h0cnbmfcn93xm5dg2x27ixhag1cwndga-glibc-2.34-210-bin
+	// example output:
+	//  version: "-2.34-210"
+	//  major: "2"
+	//  minor: "34"
+	//  patch: "210"
+	rightMostVersionIshPattern = regexp.MustCompile(`-(?P<version>(?P<major>[0-9][a-zA-Z0-9]*)(\.(?P<minor>[0-9][a-zA-Z0-9]*))?(\.(?P<patch>0|[1-9][a-zA-Z0-9]*)){0,3}(?:-(?P<prerelease>\d*[.a-zA-Z-][.0-9a-zA-Z-]*)*)?(?:\+(?P<metadata>[.0-9a-zA-Z-]+(?:\.[.0-9a-zA-Z-]+)*))?)`)
+)
+
+type Cataloger struct{}
+
+func NewStoreCataloger() *Cataloger {
+	return &Cataloger{}
+}
+
+func (c *Cataloger) Name() string {
+	return catalogerName
+}
+
+func (c *Cataloger) Catalog(resolver source.FileResolver) ([]pkg.Package, []artifact.Relationship, error) {
+	// we want to search for only directories, which isn't possible via the stereoscope API, so we need to apply the glob manually on all returned paths
+	var pkgs []pkg.Package
+	for storeLocation := range resolver.AllLocations() {
+		matches, err := doublestar.Match(nixStoreGlob, storeLocation.RealPath)
+		if err != nil {
+			return nil, nil, fmt.Errorf("failed to match nix store path: %w", err)
+		}
+		if !matches {
+			continue
+		}
+
+		storePath := parseNixStorePath(storeLocation.RealPath)
+
+		if storePath == nil || !storePath.isValidPackage() {
+			continue
+		}
+
+		pkgs = append(pkgs, newNixStorePackage(*storePath, storeLocation))
+	}
+	return pkgs, nil, nil
+}
--- syft/pkg/nix_store_metadata.go
+++ syft/pkg/nix_store_metadata.go
@@ -1,37 +1,9 @@
 package pkg
 
-import (
-	"github.com/anchore/packageurl-go"
-	"github.com/anchore/syft/syft/linux"
-)
-
-const NixStoreGlob = "/nix/store/**"
-
 type NixStoreMetadata struct {
-	Package       string           `mapstructure:"Package" json:"package"`
-	Architecture  string           `mapstructure:"A" json:"architecture"`
-	Source        string           `mapstructure:"Source" json:"source"`
-	Version       string           `mapstructure:"Version" json:"version"`
-	SourceVersion string           `mapstructure:"SourceVersion" json:"sourceVersion"`
-}
+	// Hash is the prefix of the nix store basename path
+	Hash string `mapstructure:"hash" json:"hash"`
 
-func (m NixStoreMetadata) PackageURL(d *linux.Release) string {
-	if d == nil {
-		return ""
-	}
-	pURL := packageurl.NewPackageURL(
-		// TODO: replace with `packageurl.TypeDebian` upon merge of https://github.com/package-url/packageurl-go/pull/21
-		// TODO: or, since we're now using an Anchore fork of this module, we could do this sooner.
-		"nix",
-		"",
-		m.Package,
-		m.Version,
-		packageurl.Qualifiers{
-			{
-				Key:   "arch",
-				Value: m.Architecture,
-			},
-		},
-		"")
-	return pURL.ToString()
+	// Output allows for optionally specifying the specific nix package output this package represents (for packages that support multiple outputs)
+	Output string `mapstructure:"output" json:"output,omitempty"`
 }
